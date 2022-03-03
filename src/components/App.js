a3f51ac21f33b9d035dd676c3f8119faf2d3154e
Lyssie <lyssiewright.15@gmail.com>
2022-03-03 12:35:05 -0600

    commit tree 2ab779984a62fee06471d4284fff822fd1934d08

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useState, useEffect} from "react";
+
+
+
+function App() {
+
+    const [dogs, setDogs] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+          .then(response => response.json())
+          .then((dogs) => {
+              setDogs(dogs)
+              setIsLoaded(true)
+          })},[])
+
+          if (!isLoaded) return <p>Loading...</p>
+          console.log(dogs)
+    return (
+
+      <img src={dogs.message} alt="A Random Dog"/>)
+    }
+    
+    export default App;
